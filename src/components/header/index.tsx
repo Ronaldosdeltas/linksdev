d61f37e991b39b02737b7c0a1aa49aec4d36f48f
Ronaldosdeltas <ronaldosssantos1@gmail.com>
2025-08-06 05:56:53 -0300

    creatin button page admin, using use uselocation for makin the button show only in page admin

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,18 @@
 
 import { Link } from "react-router-dom";
+import { BiLogOut } from "react-icons/bi";
+import { auth } from "../../services/firebaseconection";
+import { signOut } from "firebase/auth";
+import { useLocation } from "react-router-dom";
 
-export function Header() {
+export function Header(){
+  const location = useLocation()
+  const isAdminpage = location.pathname === '/admin'
+
+   async function handlelogout(){
+  await signOut(auth)
+
+}
   return (
     <header className="bg-gray-900 w-full py-6 flex items-center justify-between px-5">
       <Link to="/">
@@ -9,10 +20,18 @@ export function Header() {
         <span className="bg-gradient-to-r from-blue-700 to-purple-500 bg-clip-text text-transparent  hover:text-amber-300">-deltass</span>
       </h1>
       </Link>
-      <nav>
+      <nav className="flex items-center space-x-4">
         <Link to="/Contact" className="text-amber-50 hover:text-amber-300 text-2xl font-bold">
         WhatsApp
         </Link>
+
+        {isAdminpage && (
+        <button 
+        className="text-white hover:text-amber-300 hover:bg-gray-700 rounded-full p-2 transition duration-300"
+        onClick={handlelogout}>
+          <BiLogOut size={28} color= '#FFF' />
+        </button>
+        )}
    
       </nav>
     </header>
